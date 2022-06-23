f8a60ca2b6c9620fd50cdea1ba080e8e92daecd5
nose <nose>
2022-06-23 09:01:51 -0300

    gfg

--- Pagina/devolver.php
+++ Pagina/devolver.php
@@ -6,14 +6,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Modificaciones</title>
+  <!-- link bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- estilos -->
   <link rel="stylesheet" href="estilos/estilos2.css">
   <link rel="stylesheet" href="estilos/estilos_all_pages.css">
 </head>
 
 <body>
   <section class="nav-conteiner">
+    <!-- barra de busqueda -->
     <nav class="nav-btn">
       <ul class="nav nav-pills">
         <li class="nav-item">
@@ -36,11 +39,14 @@
         </li>
       </ul>
     </nav>
+    <!-- titulo de la pagina en la barra de busqueda -->
     <article class="nav-title">
       <h1>Base de datos</h1>
     </article>
   </section>
   <section class="main-conteiner">
+    <!-- contenido de la pagina. va a tener lo necesario para devolver que es:
+  que devolver?(buscador), quien devolvio?, un resultado que diga que devolver y donde -->
     <main>
       <h2 style="margin-top: 0;">Devolver Algo</h2>
       
--- Pagina/index.php
+++ Pagina/index.php
@@ -6,17 +6,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Base de datos</title>
+  <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- estilos -->
   <link rel="stylesheet" href="estilos/estilos2.css">
   <link rel="stylesheet" href="estilos/estilos_all_pages.css">
 </head>
 
 <body>
+  <!-- conteiner del nav/barra de navegacion -->
   <section class="nav-conteiner">
+    <!--Barra de busqueda-->
     <nav class="nav-btn">
-      <!-- <a href="modificiaciones.html"><input type="button" value="Modificaciones"></a>
-      <a><input type="button" value="Sacar Items"></a> -->
       <ul class="nav nav-pills">
         <li class="nav-item">
           <a class="nav-link active" href="index.php">Buscar</a>
@@ -38,16 +40,20 @@
         </li>
       </ul>
     </nav>
+    <!-- titulo de la pagina en el nav -->
     <article class="nav-title">
       <h1>Base de datos</h1>
     </article>
   </section>
+  <!-- contenedor del contenedor main con todo el contenido de la pagina -->
   <section class="main-conteiner">
+    <!-- contenedor main.  -->
     <main>
   <h2>Buscar Item</h2>
 </main>
 </section>
-  <!-- JavaScript Bundle with Popper -->
+
+<!-- codigo JS de bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
     crossorigin="anonymous"></script>
